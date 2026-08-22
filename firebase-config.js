31ccae272d5dd12822112c232e58faf65474a185
webappbyzakki <webappbyzakki@gmail.com>
2026-08-23 03:19:05 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-export const firebaseConfig={
-apiKey:"",
-authDomain:"",
-databaseURL:"",
-projectId:"",
-storageBucket:"",
-messagingSenderId:"",
-appId:""
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTEmgi5p6-5XcI9uf7wiUbqIpgEZ1_YvM",
+  authDomain: "rsvp-sholich-aminah.firebaseapp.com",
+  databaseURL: "https://rsvp-sholich-aminah-default-rtdb.firebaseio.com",
+  projectId: "rsvp-sholich-aminah",
+  storageBucket: "rsvp-sholich-aminah.firebasestorage.app",
+  messagingSenderId: "765740637145",
+  appId: "1:765740637145:web:546daea77428dd3b6717c8",
+  measurementId: "G-WNPVG1E1TH"
 };
